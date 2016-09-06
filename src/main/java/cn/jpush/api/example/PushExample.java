81ebda7212f40378ab30ef26dcd58b2f598fe7f2
jialei.jiang <jialei.jiang@baozun.cn>
2016-09-06 17:19:27 +0800

    不记得了

--- src/main/java/cn/jpush/api/example/PushExample.java
+++ src/main/java/cn/jpush/api/example/PushExample.java
@@ -23,6 +23,7 @@ import cn.jpush.api.JPushClient;
 import cn.jpush.api.common.resp.APIConnectionException;
 import cn.jpush.api.common.resp.APIRequestException;
 import cn.jpush.api.push.PushResult;
+import cn.jpush.api.push.model.Options;
 import cn.jpush.api.push.model.Platform;
 import cn.jpush.api.push.model.PushPayload;
 import cn.jpush.api.push.model.audience.Audience;
@@ -76,8 +77,9 @@ public class PushExample {
      */
     public long sendPushAll() {
         PushPayload payload = buildPushObject_all_alias_alert(content);
-        // 极光推送由v2版本升级到v3版本后，如果不设置这个参数通知推送不了
+        //如果目标平台为 iOS 平台 需要在 options 中通过 apns_production 字段来制定推送环境。True 表示推送生产环境，False 表示要推送开发环境； 如果不指定则为推送生产环境
         payload.resetOptionsApnsProduction(true);
+        payload.resetOptionsTimeToLive(60);
         LOG.info(JSON.toJSONString(payload));
         long msgId = 0;
         try {
@@ -100,6 +102,7 @@ public class PushExample {
 
     /** 构建推送对象：所有平台，推送目标的别名为alias,通知的内容为content */
     public static PushPayload buildPushObject_all_alias_alert(String content) {
+        
         return PushPayload.newBuilder().setPlatform(Platform.all())// 所有平台
                 .setAudience(Audience.all())// 向选定人推送
                 .setNotification(Notification.alert(content))// 消息内容
@@ -108,24 +111,21 @@ public class PushExample {
 
     /** 构建推送对象：向android平台，向目标标签tag,通知标题title,内容为content */
     public static PushPayload buildPushObject_android_tag_alertWithTitle(String tag, String title, String content) {
+
         return PushPayload.newBuilder().setPlatform(Platform.android()).setAudience(Audience.tag(tag)).setNotification(Notification.android(content, title, null)).build();
     }
 
-    /*
-     * public static PushPayload buildPushObject_ios_tagAnd_alertWithExtrasAndMessage() { return
-     * PushPayload.newBuilder().setPlatform(Platform.ios()).setAudience(Audience.tag_and("tag1",
-     * "tag_all"))
-     * .setNotification(Notification.newBuilder().addPlatformNotification(IosNotification.newBuilder
-     * ().setAlert(ALERT).setBadge(5).setSound("happy.caf").addExtra("from",
-     * "JPush").build()).build()).setMessage(Message.content(MSG_CONTENT))
-     * .setOptions(Options.newBuilder().setApnsProduction(true).build()).build(); }
-     * 
-     * public static PushPayload buildPushObject_ios_audienceMore_messageWithExtras() { return
-     * PushPayload.newBuilder().setPlatform(Platform.android_ios()).setAudience(Audience.newBuilder(
-     * ).addAudienceTarget(AudienceTarget.tag("tag1",
-     * "tag2")).addAudienceTarget(AudienceTarget.alias("alias1", "alias2")).build())
-     * .setMessage(Message.newBuilder().setMsgContent(MSG_CONTENT).addExtra("from",
-     * "JPush").build()).build(); }
-     */
+
+    /*public static PushPayload buildPushObject_ios_tagAnd_alertWithExtrasAndMessage() {
+        return PushPayload.newBuilder().setPlatform(Platform.ios()).setAudience(Audience.tag_and("tag1", "tag_all",""))
+                .setNotification(Notification.newBuilder().addPlatformNotification(IosNotification.newBuilder().setAlert(ALERT).setBadge(5).setSound("happy.caf").addExtra("from", "JPush").build()).build()).setMessage(Message.content(MSG_CONTENT))
+                .setOptions(Options.newBuilder().setApnsProduction(true).build()).build();
+    }
+
+    public static PushPayload buildPushObject_ios_audienceMore_messageWithExtras() {
+        return PushPayload.newBuilder().setPlatform(Platform.android_ios()).setAudience(Audience.newBuilder().addAudienceTarget(AudienceTarget.tag("tag1", "tag2")).addAudienceTarget(AudienceTarget.alias("alias1", "alias2")).build())
+                .setMessage(Message.newBuilder().setMsgContent(MSG_CONTENT).addExtra("from", "JPush").build()).build();
+    }*/
+     
 
 }
---
+++ src/main/java/cn/study/innerclass/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2015 Jumbomart All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information of Jumbomart. You shall not
+ * disclose such Confidential Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jumbo.
+ * 
+ * JUMBOMART MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. JUMBOMART SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
+ * 
+ */
+package cn.study.innerclass;
+
+public class Test {
+    public static void main(String[] args) {
+        Exsample ex = new Exsample();
+        Exsample.PublicInnerClass pub = ex.new PublicInnerClass();
+        pub.setPuAge(111);
+        pub.test();
+
+        
+        }
+}
